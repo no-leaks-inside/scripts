d41e9ef1d48e5c7695d30d247ca5b8b29e52ed59
A50-Victor <80767544+victor4cris@users.noreply.github.com>
2022-04-30 07:42:48 +0300

    kernel for beyond1lte

---
+++ kernel.sh
@@ -0,0 +1,178 @@
+ #
+ # Script For Building Android arm64 Kernel
+ #
+ # Copyright (c) 2018-2020 Panchajanya1999 <rsk52959@gmail.com>
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
+
+#! /bin/sh
+
+#Kernel building script
+
+
+##------------------------------------------------------##
+##----------Basic Informations, COMPULSORY--------------##
+
+# The defult directory where the kernel should be placed
+KERNEL_DIR=$PWD
+# The name of the Kernel, to name the ZIP
+if [ $CIRCLE_BRANCH == "Lineage-19.1" ]
+then
+	ZIPNAME="Andromeda-12.1"
+else 
+	ZIPNAME="Andromeda-legacy"
+fi
+
+# The name of the device for which the kernel is built
+MODEL="Galaxy S10/N10"
+
+# The codename of the device
+DEVICE="beyond1lte"
+
+# The defconfig which should be used. Get it from config.gz from
+# your device or check source
+DEFCONFIG=exynos9820-beyond0lte_defconfig
+
+# Clean source prior building. 1 is NO(default) | 0 is YES
+INCREMENTAL=0
+
+# Push ZIP to Telegram. 1 is YES | 0 is NO(default)
+
+CHATID="-1001113679979"
+
+
+
+# Build dtbo.img (select this only if your source has support to building dtbo.img)
+# 1 is YES | 0 is NO(default)
+
+# Check if we are using a dedicated CI ( Continuous Integration ), and
+# set KBUILD_BUILD_VERSION
+if [ $CI == true ]
+then
+	if [ $CIRCLECI == true ]
+	then
+		export KBUILD_BUILD_VERSION=$CIRCLE_BUILD_NUM
+	fi
+fi
+
+##------------------------------------------------------##
+##---------Do Not Touch Anything Beyond This------------##
+
+# Set a commit head
+COMMIT_HEAD=$(git log --oneline -1)
+
+# Set Date 
+DATE=$(TZ=Asia/Kolkata date +"%Y%m%d-%T")
+
+#Now Its time for other stuffs like cloning, exporting, etc
+
+function clone {
+	echo " "
+	echo "★★Cloning clang 11"
+	wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-12.0.0_r15/clang-r416183b1.tar.gz
+	mkdir clang
+	tar xvzf clang-r416183b1.tar.gz --directory=clang
+	# Toolchain Directory defaults to clang-llvm
+	TC_DIR=$PWD/clang
+	echo "★★Cloning gcc 10"
+	git clone --depth 1 https://github.com/odroid-dev/gcc-11.x-aarch64-linux-gnu gcc 
+	echo "★★Clang Done, Now Its time for AnyKernel .."
+	git clone --depth 1 --no-single-branch https://github.com/archie9211/AnyKernel2.git
+	echo "★★Cloning Kinda Done..!!!"
+}
+
+##------------------------------------------------------##
+
+function exports {
+	export KBUILD_BUILD_USER="v1ct0r"
+	export KBUILD_BUILD_HOST="circleci"
+	export ARCH=arm64
+	export SUBARCH=arm64
+	KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+	export KBUILD_COMPILER_STRING
+	PATH=$TC_DIR/bin/:$PATH
+	export CROSS_COMPILE="$PWD/gcc/bin/aarch64-linux-gnu-"
+	export PATH
+	export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
+	export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
+	export PROCS=$(nproc --all)
+}
+
+##---------------------------------------------------------##
+
+function tg_post_msg {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$2" \
+	-d "disable_web_page_preview=true" \
+	-d "parse_mode=html" \
+	-d text="$1"
+
+}
+
+##----------------------------------------------------------------##
+
+function tg_post_build {
+	curl --progress-bar -F document=@"$1" $BOT_BUILD_URL \
+	-F chat_id="$2"  \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=html" \
+	-F caption="$3"  
+}
+
+##----------------------------------------------------------##
+
+function build_kernel {
+
+	tg_post_msg "<b>$CIRCLE_BUILD_NUM CI Build Triggered</b>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>CircleCI</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CIRCLE_BRANCH</code>%0A<b>Top Commit : </b><code>$COMMIT_HEAD</code>%0A<b>Status : </b>#Nightly" "$CHATID"
+
+
+	make O=out $DEFCONFIG
+
+	BUILD_START=$(date +"%s")
+	make -j$PROCS O=out \
+		CROSS_COMPILE=$CROSS_COMPILE \
+		CC=clang  
+		2>&1 | tee error.log
+	
+	BUILD_END=$(date +"%s")
+	DIFF=$((BUILD_END - BUILD_START))
+	check_img
+}
+
+##-------------------------------------------------------------##
+
+function check_img {
+	if [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb ] 
+	    then
+		gen_zip
+	else		
+		tg_post_build error.log "$CHNLID" "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
+	fi
+}
+
+##--------------------------------------------------------------##
+
+function gen_zip {
+	mv $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb AnyKernel2/Image.gz-dtb
+	cd AnyKernel2
+	zip -r9 $ZIPNAME-$DEVICE-$DATE * -x .git README.md	
+	MD5CHECK=$(md5sum $ZIPNAME-$DEVICE-$DATE.zip | cut -d' ' -f1)	
+	tg_post_build $ZIPNAME-$DEVICE-$DATE.zip "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s) | MD5 Checksum : <code>$MD5CHECK</code>"
+	cd ..
+}
+
+clone
+exports
+build_kernel
+
+##----------------*****-----------------------------##
